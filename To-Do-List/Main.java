8ad5615d8e4812ae12cb661166f09aee93974930
questofjoyy <questofjoyy@gmail.com>
2025-05-28 01:43:44 +0900

    added delete method baebeeeeeeeeee

--- To-Do-List/Main.java
+++ To-Do-List/Main.java
@@ -17,11 +17,9 @@ public class Main {
 	// loads file and data. also creates the file if the file is not detected.
 	static void loadFile() {
 		try {
-			File listOfTask = new File(FILE_PATH);
-			if (listOfTask.createNewFile()) {
-				System.out.println("Required file created: " + listOfTask.getName());
-			} else {
-				System.out.println("File exists");
+			File file = new File(FILE_PATH);
+			if (file.createNewFile()) {
+				System.out.println("Required file created: " + file.getName());
 			}
 		} catch (IOException e) {
 			System.out.println("An error occured");
@@ -30,7 +28,6 @@ public class Main {
 	}
 
 	static void loadTask() {
-
 		try (
 			FileReader rof = new FileReader(FILE_PATH);
 			BufferedReader brof = new BufferedReader(rof)) {
@@ -38,7 +35,7 @@ public class Main {
 			lineNum=0;
 
 			while ((line = brof.readLine()) != null) {
-				listOfTasks[lineNum] = (lineNum + 1) + ". " + line + "\n";
+				listOfTasks[lineNum] = line;
 				lineNum++;
 			}
 		} catch (IOException e) {
@@ -48,8 +45,8 @@ public class Main {
 
 	static void writeToFile(String task) {
 		try (
-				FileWriter wtf = new FileWriter(FILE_PATH, true);
-				BufferedWriter bw = new BufferedWriter(wtf)) {
+			FileWriter wtf = new FileWriter(FILE_PATH, true);
+			BufferedWriter bw = new BufferedWriter(wtf)) {
 			bw.write(task);
 			bw.newLine();
 		} catch (IOException e) {
@@ -62,21 +59,59 @@ public class Main {
 	static void readFromFile(String FILE_PATH) {
 		loadTask();
 		System.out.println("\n\nYour List of Tasks");
-		System.out.println("===============");
+		System.out.println("----------------------");
 		for (int i = 0; i < lineNum; i++) {
-			System.out.printf("%s", listOfTasks[i]);
+			System.out.printf("%d. %s\n",i+1, listOfTasks[i]);
 		}
-		System.out.println("===============\n\n");
+		System.out.println("----------------------\n\n");
 	}
 
-	static void deleteTask ()
-	{
-		loadTask();
+	////
+	static void deleteTask (int deleteLine) {
+		lineNum = 0;
+		try (
+			FileReader rof = new FileReader(FILE_PATH);
+			BufferedReader brof = new BufferedReader(rof)) {
+			String line;
+			int currentLine=0;
 
+			while ((line = brof.readLine()) != null) {
+				if(currentLine == deleteLine - 1) {
+					currentLine++;
+					continue;
+				}
+				listOfTasks[lineNum] = line;
+				lineNum++;
+				currentLine++;
+			}
+		} catch (IOException e) {
+			System.err.println("Error reading file: " + e.getMessage());
+		}
+
+
+		try (FileWriter fw = new FileWriter(FILE_PATH, false)) {
+
+		} catch (IOException e) {
+			System.err.println("Error clearing file: " + e.getMessage());
+		}
+
+		try (
+			FileWriter wtf = new FileWriter(FILE_PATH, true);
+			BufferedWriter bw = new BufferedWriter(wtf)) {
+			for(int i = 0; i < lineNum; i++) {
+			bw.write(listOfTasks[i]);
+			bw.newLine();
+			}
+		} catch (IOException e) {
+			System.err.println("Error reading file: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		System.out.println("Task deleted Successfully!!!");
 	}
 
-			public static void main(String[] args) {
 
+	public static void main(String[] args) {
 		loadFile();
 		loadTask();
 
@@ -85,11 +120,14 @@ public class Main {
 		int inp;
 
 		while (running) {
-			System.out.println("To-do-list");
+			System.out.println("\n===============");
+			System.out.println("  To-do-list");
+			System.out.println("===============");
 			System.out.println("1. Add Tasks");
 			System.out.println("2. Delete Tasks");
 			System.out.println("3. View Tasks");
 			System.out.println("4. Exit");
+			System.out.println("---------------\n");
 			System.out.printf("Enter an option: ");
 
 			inp = scan.nextInt();
@@ -104,7 +142,8 @@ public class Main {
 					break;
 				case 2:
 					System.out.printf("Enter the task you want to delete: ");
-					deleteTask();
+					int deleteLine = scan.nextInt();
+					deleteTask(deleteLine);
 					break;
 				case 3:
 					readFromFile(FILE_PATH);
@@ -120,6 +159,5 @@ public class Main {
 
 		}
 		scan.close();
-
 	}
 }
