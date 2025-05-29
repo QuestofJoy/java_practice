528441144e2e9d2191243e758b3acc4046aa90bd
questofjoyy <questofjoyy@gmail.com>
2025-05-30 04:09:34 +0900

    finally a shitty halffinished hangman lesgooooo

--- HangMan/Main.java
+++ HangMan/Main.java
@@ -1,8 +1,48 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.List;
 
 class Main {
+
+  static int chance = 5;
+
+  static void wordChecker(String playerWord, char usrInput) {
+    playerWord = playerWord.toLowerCase();
+    int wordLength = playerWord.length();
+    boolean correct = false;
+    char[] blanks = new char[100];
+    // just for checking all the words are converted to lowerCase
+
+    for (int i = 0; i < wordLength; i++) {
+      if (playerWord.charAt(i) == usrInput) {
+        blanks[i] = usrInput;
+        System.out.println("Correct!!!");
+        correct = true;
+      } else {
+        blanks[i] = '_';
+      }
+    }
+    if (!correct) {
+      chance--;
+    }
+
+    for (int i = 0; i < wordLength; i++) {
+      System.out.printf("%c", blanks[i]);
+    }
+    System.out.println();
+
+  }
+
+  static void tui(String playerWord) {
+  }
+
+  static void tuiUpdater(char usrInput) {
+    // TODO : make this shit work. usrinput is working flawless. left off is
+    // update
+    // the tui after
+    // the user inputs right char
+  }
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
@@ -21,16 +61,19 @@ class Main {
     hints.add(3, "Final destiny");
 
     int rand = random.nextInt(3);
+    String playerWord = words.get(rand);
 
-    //
     System.out.println("Welcome to hangman game!!!");
 
     System.out.println("Your word is: " + hints.get(rand));
-    char usrInput = scan.next().charAt(0);
 
-    System.out.println(usrInput);
-
+    while (chance > 0) {
+      tui(playerWord);
+      System.out.print("Enter your guess word: ");
+      char usrInput = scan.next().charAt(0);
+      usrInput = Character.toLowerCase(usrInput);
+      wordChecker(playerWord, usrInput);
+    }
     scan.close();
-
   }
 }
