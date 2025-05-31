683e30223238604c6ae7c80131abda62a6011f47
questofjoy <questofjoyy@gmail.com>
2025-06-01 00:02:22 +0900

    game complete. optimization is left off for the future me

--- HangMan/Main.java
+++ HangMan/Main.java
@@ -1,78 +1,73 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.util.ArrayList;
-import java.util.List;
 
 class Main {
 
   static int chance = 5;
 
-  static void wordChecker(String playerWord, char usrInput) {
+  static boolean inpChecker(String playerWord, char[] blanks, char usrInput) {
+    boolean found = false;
     playerWord = playerWord.toLowerCase();
-    int wordLength = playerWord.length();
-    boolean correct = false;
-    char[] blanks = new char[100];
-    // just for checking all the words are converted to lowerCase
 
-    for (int i = 0; i < wordLength; i++) {
-      if (playerWord.charAt(i) == usrInput) {
-        blanks[i] = usrInput;
-        System.out.println("Correct!!!");
-        correct = true;
-      } else {
-        blanks[i] = '_';
+    for (int i = 0; i < playerWord.length(); i++) {
+      if (usrInput == playerWord.charAt(i)) {
+        blanks[i] = playerWord.charAt(i);
+        found = true;
       }
     }
-    if (!correct) {
+
+    if (!found) {
+      System.out.println("word not found");
       chance--;
+    } else {
+      System.out.println("word found");
     }
+    String usrInputWord = new String(blanks);
+    System.out.println(usrInputWord);
 
-    for (int i = 0; i < wordLength; i++) {
-      System.out.printf("%c", blanks[i]);
-    }
-    System.out.println();
-
-  }
-
-  static void tui(String playerWord) {
-  }
-
-  static void tuiUpdater(char usrInput) {
-    // TODO : make this shit work. usrinput is working flawless. left off is
-    // update
-    // the tui after
-    // the user inputs right char
+    boolean clear = false;
+    clear = usrInputWord.equals(playerWord);
+    return clear;
   }
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     Random random = new Random(System.currentTimeMillis());
 
-    ArrayList<String> words = new ArrayList<>();
-    words.add(0, "HelloWorld");
-    words.add(1, "Java");
-    words.add(2, "Elephant");
-    words.add(3, "Heaven");
+    String[] words = new String[100];
+    words[0] = "HelloWorld";
+    words[1] = "Java";
+    words[2] = "Elephant";
+    words[3] = "Heaven";
 
-    ArrayList<String> hints = new ArrayList<>();
-    hints.add(0, "First word of a programmer.");
-    hints.add(1, "Programming language that this program in used to write.");
-    hints.add(2, "Largest land mammal.");
-    hints.add(3, "Final destiny");
+    String[] hints = new String[100];
+    hints[0] = "First word of a programmer.";
+    hints[1] = "Programming language that this program in used to write.";
+    hints[2] = "Largest land mammal.";
+    hints[3] = "Final destiny";
 
-    int rand = random.nextInt(3);
-    String playerWord = words.get(rand);
+    int rand = random.nextInt(4);
+    String playerWord = words[rand];
+    boolean wordClear = false;
+
+    char[] blanks = new char[playerWord.length()];
+    for (int i = 0; i < playerWord.length(); i++) {
+      blanks[i] = '_';
+    }
 
     System.out.println("Welcome to hangman game!!!");
+    System.out.println("Your word is: " + hints[rand]);
+    System.out.println(blanks);
 
-    System.out.println("Your word is: " + hints.get(rand));
-
-    while (chance > 0) {
-      tui(playerWord);
-      System.out.print("Enter your guess word: ");
+    while (chance > 0 && !wordClear) {
+      System.out.printf("Enter your guess: ");
       char usrInput = scan.next().charAt(0);
-      usrInput = Character.toLowerCase(usrInput);
-      wordChecker(playerWord, usrInput);
+      wordClear = inpChecker(playerWord, blanks, usrInput);
+    }
+    if (chance == 0) {
+      System.out.println("nice try. Better LUCK NEXT FUCKING TIMEE 󱁆");
+    } else {
+      System.out.println("well done. congratulations. yayy󱥋󱥋󱥋");
     }
     scan.close();
   }
