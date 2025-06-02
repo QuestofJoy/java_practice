3964880d24b9efa935bf9abe2e76a6e3160ff0c0
questofjoy <questofjoyy@gmail.com>
2025-06-03 03:01:37 +0900

    new features incomingg

--- HangMan/GameLogic.java
+++ HangMan/GameLogic.java
@@ -2,6 +2,6 @@ class GameLogic {
   // load game dataa
   // i didnt have to create and write to fiels but lets learn
 
-  InitializeData ig;
+  InitializeDataAndFolder ig;
 
 }
--- HangMan/Main.java
+++ HangMan/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
   public static void main(String[] args) {
-    InitializeData ig = new InitializeData();
+    InitializeDataAndFolder ig = new InitializeDataAndFolder();
   }
 }
